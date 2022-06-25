ea1a4033e13fda4785ef197b620bb6aed1b509f2
Varad Thokal <varadthokal3@gmail.com>
2022-06-25 20:55:02 +0530

    Update printAllSubsequence.cpp

--- recursion/printAllSubsequence.cpp
+++ recursion/printAllSubsequence.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-void subseq(vector<int>a,vector<int>res,int ind) {
+void subseq(vector<int>&a,vector<int>&res,int ind) {
     if (ind == 3) {
         for(auto i : res) {
             cout<<i<<" ";
@@ -13,6 +13,9 @@ void subseq(vector<int>a,vector<int>res,int ind) {
     subseq(a,res,ind+1);     // dont't take curr element in ans vector
     res.push_back(a[ind]);  // after coming back from last permutation, include the current element in ans vector
     subseq(a,res,ind+1);    // pass the current ans vector to next call, so that permutations can now happen on this vector.
+    
+    res.pop_back();  // when coming back from a prev push_back call, pop_back so that next permutations can happen properly.
+    return;
 }
 int main() {
     vector<int>a;
