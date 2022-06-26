efaec9eae004884bab72147ad5ccd5f88d561e7b
Varad Thokal <varadthokal3@gmail.com>
2022-06-26 12:13:33 +0530

    Update countAllSubseqEqualToK.cpp

--- recursion/countAllSubseqEqualToK.cpp
+++ recursion/countAllSubseqEqualToK.cpp
@@ -1,36 +1,43 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
- 
-int subseq (vector<int>a, int ind, int csum, int &cnt) { // maintain the count or use a global variable.
-    if (ind == 4) {  // get all the subseqences here.
-        if(csum==0) { // this means sum becomes k
-            // for(auto i : res) {
-            //     cout<<i<<" ";
-            // } cout<<endl;
-            return ++cnt;  // add to count.
+
+int subseq (vector<int>&a, int ind, vector<int>&res,int &csum) {
+    int result = 0;
+    if(ind > 2) {
+        if(csum==0) {
+        result=1;
         }
-        return cnt;
+        return result;
     }
     
-   cnt = subseq(a,ind+1,csum,cnt);  // not choosing the current element
-    csum-=a[ind];   // choosing the current element. ( csum is k at the start )
-    cnt = subseq(a,ind+1,csum,cnt); 
+    int left = subseq(a,ind+1,res,csum);
+
+    res.push_back(a[ind]);
+    csum-=a[ind];
+    int right = subseq(a,ind+1,res,csum);
+    csum+=a[ind];
+    res.pop_back();
+    return left + right;  // return the count from both the calls to the prev call.
     
-    return cnt;
-   
- 
+    
+    
 }
 int main() {
     vector<int>a;
     a.push_back(1);
-    a.push_back(5);
-    a.push_back(3);
-    a.push_back(3);
-    
-    int cnt = 0;
-    int sum = 6;
-    cnt = subseq(a,0,sum,cnt);
-    cout<<cnt;
+    a.push_back(2);
+    a.push_back(1);
+
+  
+    // unsync the I/O of C and C++.
+    ios_base::sync_with_stdio(false);
+  
+    int sum = 3;
+    vector<int>res;
+    int ans = subseq(a,0,res,sum);
+    cout<<ans;
+
     return 0;
+    
 }
