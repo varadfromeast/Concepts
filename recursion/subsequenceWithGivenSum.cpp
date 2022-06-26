aac4413cdef389df2294f3956389f9be517d9907
Varad Thokal <varadthokal3@gmail.com>
2022-06-26 11:04:09 +0530

    Update subsequenceWithGivenSum.cpp

--- recursion/subsequenceWithGivenSum.cpp
+++ recursion/subsequenceWithGivenSum.cpp
@@ -1,29 +1,46 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int sum = 3; //given sum
-void subseq(vector<int>a, vector<int>res, int ind, int csum) {
-    if (ind == 3) { // go to final condition -> this is where we get all subsequences.
-        if(csum==0) { // instead of printing all subseq. print the ones which has made the sum to be 0. 
-            for(auto i : res) { // note that it is imp to go till ind == 3, because at this stage only we get all the permutations.
-                cout<<i<<" ";
-            } cout<<endl;
+
+void subseq (vector<int>&a, int ind, vector<int>&res,int &csum) {
+    if(ind > 4) {
+        if(csum==0) { // extra cond to check if sum has reached
+        for(auto i : res)
+            cout<<i<<" ";
+        cout<<endl;
         }
+  
         return;
     }
-    //same code as print all subseq, just another variable which maintains the current sum. 
-    subseq(a,res,ind+1,csum);
-    csum = csum - a[ind]; // whatever the current sum is, subtract from it current element.
-    res.push_back(a[ind]);
-    subseq(a,res,ind+1,csum);
+    
+    subseq(a,ind+1,res,csum);  // ignore the current ele
+    
+    res.push_back(a[ind]);  // pick the current ele
+    csum-=a[ind];
+    subseq(a,ind+1,res,csum);
+    
+    csum+=a[ind];  // when coming back make the sum as it was 
+    res.pop_back();
+    return;
+    
+    
 }
 int main() {
     vector<int>a;
-    a.push_back(3);
     a.push_back(1);
     a.push_back(2);
+    a.push_back(3);
+    a.push_back(3);
+    a.push_back(6);
     
+  
+    // unsync the I/O of C and C++.
+    ios_base::sync_with_stdio(false);
+  
+    int sum = 6;
     vector<int>res;
-    subseq(a,res,0,sum); // give the final sum, because we are subtracting.
+    subseq(a,0,res,sum,sum);
+
     return 0;
+    
 }
